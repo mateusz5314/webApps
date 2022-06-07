998a2970363687958703aa729ed7a5e0a9db14cd
Mateusz J <mateusz5314@gmail.com>
2022-06-07 20:39:00 +0200

    add allowed hosts

--- webApps/settings.py
+++ webApps/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-u$h)=_5slnltky#rt9n!dg=2f*epfjilaqv&7%vkav93s(c0*(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
